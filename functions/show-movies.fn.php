e1b5c7578607777c04db77a24eec257b11c88db1
nsiderwolverine <justin.joseph0@yahoo.com>
2016-08-27 16:26:42 +0000

    Display a single movie

--- functions/show-movies.fn.php
+++ functions/show-movies.fn.php
@@ -8,7 +8,7 @@ function showMovies($data) {
   switch ($data) {
     case 'favorites':
       $stmt = $db->prepare("SELECT *
-                            FROM movies
+                            FROM `movies`
                             WHERE `movie_id` IN (
                               SELECT `movie_id`
                               FROM `favourites`
@@ -19,13 +19,19 @@ function showMovies($data) {
       break;
     case 'non-favorites':
       $stmt = $db->prepare("SELECT *
-                            FROM movies
+                            FROM `movies`
                             WHERE `movie_id` NOT IN (
                               SELECT `movie_id`
                               FROM `favourites`
                               WHERE `favourites`.`user_id` = ?)
                             ORDER BY `title`");
       $stmt->bind_param('i', $user_id);
+      break;
+    case 'single':
+      $stmt = $db->prepare("SELECT *
+                            FROM `movies`
+                            WHERE `movie_id` = ?");
+      $stmt->bind_param('i', $movie_id);
       break;
   }
   
@@ -44,23 +50,42 @@ function showMovies($data) {
     
     $output .= '<li>';
     
-    if ( $data == 'favorites' ) {
-      $output .= '<a href="/?user_id=' . $user_id . '&amp;movie_id=' . $movie_id . '">' . $title . '</a>';
-    } else {
-      if ( file_exists('img/movies/' . $id . '-tn.png')) {
-        $thumbnail = 'img/movies/' . $id . '-tn.png';
-      } else {
-        $thumbnail = 'img/movies/generic-tn.png';
-      }
-      
-      $output .= '<figure>';
-      $output .= '<a href="/?movie_id=' . $id . '&amp;user_id=' . $user_id . '"><img src=' . $thumbnail . ' class="thumbnail" alt="Thumbnail"></a>';
-      $output .= '<figcaption>';
-      $output .= '<h3><a href="/?movie_id=' . $id . '&amp;user_id=' . $user_id . '">' . $title . '</a></h3>';
-      $output .= '<div class="description">' . $description . '</div>';
-      $output .= '<div class="add-remove favorite"></div>';
-      $output .= '</figcaption>';
-      $output .= '</figure>';
+    switch ( $data ) {
+      case 'favorites':
+        $output .= '<a href="/?user_id=' . $user_id . '&amp;movie_id=' . $movie_id . '">' . $title . '</a>';
+        break;
+      case 'non-favorites':
+        if ( file_exists('img/movies/' . $id . '-tn.png')) {
+          $thumbnail = 'img/movies/' . $id . '-tn.png';
+        } else {
+          $thumbnail = 'img/movies/generic-tn.png';
+        }
+        
+        $output .= '<figure>';
+        $output .= '<a href="/?movie_id=' . $id . '&amp;user_id=' . $user_id . '"><img src=' . $thumbnail . ' class="thumbnail" alt="Thumbnail"></a>';
+        $output .= '<figcaption>';
+        $output .= '<h3><a href="/?movie_id=' . $id . '&amp;user_id=' . $user_id . '">' . $title . '</a></h3>';
+        $output .= '<div class="description">' . $description . '</div>';
+        $output .= '<div class="add-remove favorite"></div>';
+        $output .= '</figcaption>';
+        $output .= '</figure>';
+        break;
+      case 'single':
+        if ( file_exists('img/movies/' . $id . '-tn.png') ) {
+          $thumbnail = 'img/movies/' . $id . '-tn.png';
+        } else {
+          $thumbnail = 'img/movies/generic-tn.png';
+        }
+        
+        $output .= '<img class="movie-player" alt=' . $title . ' src=' . $thumbnail . '>';
+        $output .= '<h3>' . $title . '</h3>';
+        $output .= '<div class="actions>';
+        $output .= '<div class="add-remove">';
+        $output .= '<p>Add To/Remove from Favorites</p>';
+        $output .= '</div>';
+        $output .= '</div>';
+        $output .= '<p class="description">' . $description . '</p>';
+        break;
     }
     
     $output .= '</li>';
--- includes/single-movie.inc.php
+++ includes/single-movie.inc.php
@@ -1,10 +1,13 @@
+<?php $movie = showMovies('single'); ?>
+
     <section class="single-movie">
-      <a href="#"><img class="movie-player" alt="Movie Title" src="img/movies/generic.png"></a>
+      <?php echo $movie; ?>
+      <!--<a href="#"><img class="movie-player" alt="Movie Title" src="img/movies/generic.png"></a>
       <h3>Movie Title</h3>
       <div class="actions">
         <div class="add-remove">
           <p>Add to/remove from favourites</p>
         </div>
       </div>
-      <p class="description">Description</p>
+      <p class="description">Description</p>-->
     </section>
